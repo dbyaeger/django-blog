4eabd7896a081b4d7b7e435ce1bd5a30e0e55a8f
Daniel Yaeger <danielyaeger@Daniels-MacBook-Air.local>
2024-01-04 14:26:57 -0800

    Replaced function-based views with class-based views

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -13,17 +15,26 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type='text/plain')
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    post = published.order_by('-published_date')
-    context = {'posts': post}
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
+
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     post = published.order_by('-published_date')
+#     context = {'post_list': post}
+#     return render(request, 'blogging/list.html', context)
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
